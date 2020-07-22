2a9fe63004647bd690d04ca80556e67edaf0cec9
Jeremy <Jeremymezon@gmail.com>
2020-07-22 19:21:41 -0400

    Primer commit react.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment} from 'react';
+import Experiencias from './experiencias'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = ()=> (
+  <Fragment>
+    <div>
+      <h1 className="display-2 text-center m-5">Hola Mundo</h1>
     </div>
-  );
-}
+
+    <div className="row w-75 mx-auto d-flex justify-content-center my-5">
+      <Experiencias/>
+      <Experiencias/>
+      <Experiencias/>
+      <Experiencias/>
+      <Experiencias/>
+      <Experiencias/>
+    </div>
+    
+  </Fragment>
+)
 
 export default App;
+
+/*
+Reglas JSX
+
+1. Las etiquetas deben cerrarse siempre
+2. Los componentes deben tener solo una etiqueta padre
+3. Cuando se llama un componente para renderizar este siempre debe empezar con mayusculas
+4. Usar Fragments a la hora de usar mas de 2 elementos en un mismo componente
+
+*/
---
+++ src/experiencias.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Experiencias = ()=> (
+    <div className="col-12 col-md-6 col-lg-4 p-3">
+        <div className="card">
+            <img className="card-img-top" src="https://images.pexels.com/photos/807598/pexels-photo-807598.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="Card image cap"/>
+            <div className="card-body">
+                <h5 className="card-title">Titulo</h5>
+                <p className="card-text">Un pequeño parrafo acerca de esta tarjeta</p>
+                <a href="https://www.pexels.com/es-es/foto/agua-crecimiento-exuberante-fondo-de-pantalla-4k-807598/" className="btn btn-primary">Mas informacion</a>
+            </div>
+        </div>
+    </div>    
+)
+
+export default Experiencias;
